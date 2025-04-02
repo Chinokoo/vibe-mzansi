d18d4542389d48322e1b7c1e67626bcc836ade2b
Peter <peterchinokoo@gmail.com>
2025-04-02 18:43:28 +0200

    Fix the navbar not showing when user is not logged in

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,4 @@
+"use server";
 import Link from "next/link";
 import React from "react";
 import DesktopNav from "./DesktopNav";
@@ -5,10 +6,44 @@ import Image from "next/image";
 import MobileNav from "./MobileNav";
 import { currentUser } from "@clerk/nextjs/server";
 import { syncUser } from "@/actions/user.actions";
+import { SignIn, SignInButton } from "@clerk/nextjs";
+import { Button } from "./ui/button";
+import { ModeToggle } from "./theme_button";
 
 const NavBar = async () => {
   const user = await currentUser();
-  if (!user) return;
+  if (!user)
+    return (
+      <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blu supports-[backdrop-filter]:bg-background/60 z-50">
+        {/* container inside the nav*/}
+        <div className="max-w-7xl mx-auto px-4">
+          {/* two containers */}
+          <div className="flex items-center justify-between h-16">
+            {/* first container for image */}
+            <div className="relative w-10 h-10 flex items-center justify-center">
+              <Link href={"/"}>
+                <Image
+                  src="/assets/sa-rounded.png"
+                  fill
+                  alt="south african flag"
+                  className="object-contain"
+                />
+              </Link>
+            </div>
+            <div>
+              <ModeToggle />
+              {/* second container for navigation either desktop or mobile */}
+              <SignInButton mode="modal">
+                <Button variant="default" size={"lg"} className="ml-4 py-4">
+                  {" "}
+                  Sign In{" "}
+                </Button>
+              </SignInButton>
+            </div>
+          </div>
+        </div>
+      </nav>
+    );
 
   const userId = user.id;
   const username = user.emailAddresses[0].emailAddress.split("@")[0];
